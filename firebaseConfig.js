8f08d1de599de115e234193cdbea926d4df35e6f
Michal Bienias <131092094+MichalBienias@users.noreply.github.com>
2025-02-04 12:05:04 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,15 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyAYFqeR9ynvESnDGNBH7S0lDajMByuf-DM",
+  authDomain: "mypa-f26e8.firebaseapp.com",
+  projectId: "mypa-f26e8",
+  storageBucket: "mypa-f26e8.firebasestorage.app",
+  messagingSenderId: "1015804360651",
+  appId: "1:1015804360651:web:1e7ef9381ed53d0801684e",
+  measurementId: "G-K5M7F4DJET"
 };
 
 export const app = initializeApp(firebaseConfig);
